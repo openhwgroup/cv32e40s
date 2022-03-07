79c0289b24e829f5fbffdf1f0b5363c686e63af3
Oivind Ekelund <oivind.ekelund@silabs.com>
2022-03-07 14:56:39 +0100

    Fix type for parameter PMP_PMPADDR_RV and width of reset value used for pmp_addr_csr_i
    Signed-off-by: Oivind Ekelund <oivind.ekelund@silabs.com>

--- rtl/cv32e40s_core.sv
+++ rtl/cv32e40s_core.sv
@@ -44,7 +44,7 @@ module cv32e40s_core import cv32e40s_pkg::*;
   parameter int          PMP_GRANULARITY                     =  0,
   parameter int          PMP_NUM_REGIONS                     =  0,
   parameter pmpncfg_t    PMP_PMPNCFG_RV[PMP_NUM_REGIONS-1:0] = '{default:PMPNCFG_DEFAULT},
-  parameter [31:0]       PMP_PMPADDR_RV[PMP_NUM_REGIONS-1:0] = '{default:32'h0},
+  parameter logic [31:0] PMP_PMPADDR_RV[PMP_NUM_REGIONS-1:0] = '{default:32'h0},
   parameter mseccfg_t    PMP_MSECCFG_RV                      = MSECCFG_DEFAULT,
   parameter lfsr_cfg_t   LFSR0_CFG                           = LFSR_CFG_DEFAULT, // Do not use default value for LFSR configuration
   parameter lfsr_cfg_t   LFSR1_CFG                           = LFSR_CFG_DEFAULT, // Do not use default value for LFSR configuration
