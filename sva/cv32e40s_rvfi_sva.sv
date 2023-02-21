3fd10ea3af21003565a1036b7a024940c293b55f
Oystein Knauserud <Oystein.Knauserud@silabs.com>
2023-02-21 09:01:16 +0100

    Revert "Added a fourth cause_type for rvfi_trap for misaligned atomics."
    This reverts commit 725de444420150fd1ae56629d032cebfb6fe0bb1.
    
    Signed-off-by: Oystein Knauserud <Oystein.Knauserud@silabs.com>

--- sva/cv32e40s_rvfi_sva.sv
+++ sva/cv32e40s_rvfi_sva.sv
@@ -264,7 +264,7 @@ if ((A_EXT == A) || (A_EXT == ZALRSC)) begin
                   lsu_split_q_wb_i
                   |=>
                   rvfi_valid &&
-                  (rvfi_trap.cause_type == MEM_ERR_MISALIGNED_ATOMIC) &&
+                  (rvfi_trap.cause_type == MEM_ERR_IO_ALIGN) &&
                   (rvfi_trap.exception_cause == EXC_CAUSE_LOAD_FAULT))
     else `uvm_error("rvfi", "Exception on misaligned LR.W atomic instruction did not set correct cause_type in rvfi_trap")
 
@@ -284,9 +284,9 @@ if ((A_EXT == A) || (A_EXT == ZALRSC)) begin
                   lsu_split_q_wb_i
                   |=>
                   rvfi_valid &&
-                  (rvfi_trap.cause_type == MEM_ERR_MISALIGNED_ATOMIC) &&
+                  (rvfi_trap.cause_type == MEM_ERR_IO_ALIGN) &&
                   (rvfi_trap.exception_cause == EXC_CAUSE_STORE_FAULT))
-    else `uvm_error("rvfi", "Exception on misaligned SC.W atomic instruction did not set correct cause_type in rvfi_trap")
+    else `uvm_error("rvfi", "Exception on aligned SC.W atomic instruction did not set correct cause_type in rvfi_trap")
 end
 
 if (A_EXT == A) begin
@@ -306,9 +306,9 @@ if (A_EXT == A) begin
                   lsu_split_q_wb_i
                   |=>
                   rvfi_valid &&
-                  (rvfi_trap.cause_type == MEM_ERR_MISALIGNED_ATOMIC) &&
+                  (rvfi_trap.cause_type == MEM_ERR_IO_ALIGN) &&
                   (rvfi_trap.exception_cause == EXC_CAUSE_STORE_FAULT))
-    else `uvm_error("rvfi", "Exception on misaligned AMO* atomic instruction did not set correct cause_type in rvfi_trap")
+    else `uvm_error("rvfi", "Exception on aligned AMO* atomic instruction did not set correct cause_type in rvfi_trap")
 end
 
   /* TODO: Add back in.
